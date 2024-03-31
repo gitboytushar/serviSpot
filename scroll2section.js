9438bde82beb81dc938ae6183f8a68c504b8d80a
Tushar Verma <tusharwork.001@gmail.com>
2024-03-31 12:03:38 +0530

    Updates: Responsiveness, Logo, Content Tweaks
    Website is now Tablet Responsive

--- scroll2section.js
+++ scroll2section.js
@@ -1,3 +1,17 @@
+function scrollToHome(home) {
+  var section = document.getElementById('home');
+  var navbarHeight = document.querySelector('.main-nav').offsetHeight;
+  var distanceFromTop = 15; // Adjust this value as needed
+
+  if (section) {
+    var offset = section.getBoundingClientRect().top + window.pageYOffset - navbarHeight - distanceFromTop;
+    window.scrollTo({
+      top: offset,
+      behavior: 'smooth'
+    });
+  }
+}
+
 function scrollToSection1(section1) {
   var section = document.getElementById('section1');
   var navbarHeight = document.querySelector('.main-nav').offsetHeight;

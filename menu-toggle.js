4566cf7bdee7418566914fe40ece818390773ef1
Tushar Verma <tusharwork.001@gmail.com>
2024-04-18 00:04:07 +0530

    Minor Tweaks in navbar Menu
    menu-icon can change into X-icon, background changed

--- menu-toggle.js
+++ menu-toggle.js
@@ -1,34 +1,24 @@
-// const toggleButton = document.getElementById('menu-icon')
-// const navLinks = document.getElementById('menu')
-// const navLinksList = navLinks.querySelectorAll('a')
+const toggleButton = document.getElementById('menu-icon')
+const navLinks = document.getElementById('menu')
+const navLinksList = navLinks.querySelectorAll('a')
 
-// toggleButton.addEventListener('click', () => {
-//   navLinks.classList.toggle('active')
-// })
-
-// navLinksList.forEach(link => {
-//   link.addEventListener('click', () => {
-//     navLinks.classList.remove('active')
-//   })
-// })
-
-const toggleButton = document.getElementById('menu-icon');
-const navLinks = document.getElementById('menu');
-const navLinksList = navLinks.querySelectorAll('a');
+const menuIconImg = toggleButton.querySelector('img') // menu-icon is inside the toggleButton
 
 toggleButton.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-  // Prevent scrolling to the top when the menu icon is clicked
+  navLinks.classList.toggle('active')
   if (navLinks.classList.contains('active')) {
-    document.body.style.overflow = 'hidden'; // Disable scrolling
+    document.body.style.overflow = 'hidden' // Disable scrolling
+    menuIconImg.src = 'assets/cancel-icon.svg' // Change to cancel-icon
   } else {
-    document.body.style.overflow = ''; // Enable scrolling
+    document.body.style.overflow = '' // Enable scrolling
+    menuIconImg.src = 'assets/menu-icon.svg' // Change back to menu-icon
   }
-});
+})
 
 navLinksList.forEach(link => {
   link.addEventListener('click', () => {
-    navLinks.classList.remove('active');
-    document.body.style.overflow = ''; // Enable scrolling when a menu item is clicked
-  });
-});
+    navLinks.classList.remove('active')
+    document.body.style.overflow = '' // Enable scrolling when a navLink is clicked
+    menuIconImg.src = 'assets/menu-icon.svg' // Change back to menu=icon
+  })
+})

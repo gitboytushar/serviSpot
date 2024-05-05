1a241131051369db94e4049e29ac6f093f2f1ee3
Tushar Verma <tusharwork.001@gmail.com>
2024-05-05 13:28:18 +0530

    Imp. Bugs Fixed: Icons Quality Issues, Minor Redesigns

--- menu-toggle.js
+++ menu-toggle.js
@@ -1,6 +1,7 @@
 const toggleButton = document.getElementById('menu-icon')
 const navLinks = document.getElementById('menu')
 const navLinksList = navLinks.querySelectorAll('a')
+const menuIconSpan = toggleButton.querySelector('.hamburger-icon');
 
 const menuIconImg = toggleButton.querySelector('img') // menu-icon is inside the toggleButton
 
@@ -8,10 +9,10 @@ toggleButton.addEventListener('click', () => {
   navLinks.classList.toggle('active')
   if (navLinks.classList.contains('active')) {
     document.body.style.overflow = 'hidden' // Disable scrolling
-    menuIconImg.src = 'assets/cancel-icon.svg' // Change to cancel-icon
+    menuIconSpan.textContent = 'close'; // Change to cancel-icon
   } else {
     document.body.style.overflow = '' // Enable scrolling
-    menuIconImg.src = 'assets/menu-icon.svg' // Change back to menu-icon
+    menuIconSpan.textContent = 'menu'; // Change back to menu-icon
   }
 })
 
@@ -19,6 +20,6 @@ navLinksList.forEach(link => {
   link.addEventListener('click', () => {
     navLinks.classList.remove('active')
     document.body.style.overflow = '' // Enable scrolling when a navLink is clicked
-    menuIconImg.src = 'assets/menu-icon.svg' // Change back to menu=icon
+    menuIconSpan.textContent = 'menu'; // Change back to menu=icon
   })
 })
